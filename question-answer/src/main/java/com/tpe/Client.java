6212392daca4b53dd215843697762f6489ee738a
muhammet topcu <mamitopcu35@gmail.com>
2023-04-21 22:37:17 +0200

    client

--- question-answer/src/main/java/com/tpe/Client.java
+++ question-answer/src/main/java/com/tpe/Client.java
@@ -5,25 +5,53 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import com.tpe.domain.Question;
+import com.tpe.domain.QuestionDetail;
 import com.tpe.domain.enums.QuestionPriority;
+import com.tpe.service.IQuestionDetailService;
+import com.tpe.service.IQuestionService;
+import com.tpe.service.QuestionDetailService;
+import com.tpe.service.QuestionService;
 import com.tpe.util.HibernateUtil;
 
 public class Client {
+	
+	static IQuestionService qService = new QuestionService();
+	static IQuestionDetailService qDService = new QuestionDetailService();
+	
 	public static void main(String[] args) {
 		
 		Question question = new Question();
-		question.setName("What is Java");
+		question.setName("What is Hibernate");
 		question.setPriority(QuestionPriority.CRITICAL);
 		
 		Client client =new Client();
+//		client.saveQuestion(question);
 		
+		Question questionFound = client.findQuestion(100L);
 		
+		QuestionDetail detail = new QuestionDetail();
+		detail.setDescription("It is about oop concept");
+		detail.setQuestion(questionFound);
+		
+		client.saveQuestionDetail(detail);
 		
 		
 		
 		HibernateUtil.getSessionFactory().close();
 		
 	}
+	
+	private Question findQuestion(Long id) {
+		return qService.find(id);
+		}
+
+	private void saveQuestion(Question question) {
+		qService.save(question);
+		}
+	
+	private void saveQuestionDetail(QuestionDetail questionDetail) {
+		qDService.save(questionDetail);
+		}
 
 	
 //	public <T> void save(T t) {
